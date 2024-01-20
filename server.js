e05149fed65d9dbc22c55bffe873e1a4bf4d36ae
Yogesh Vashisth <130190342+yogeshvas@users.noreply.github.com>
2024-01-20 14:03:08 +0530

    Update server.js added messging feature

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const { Server } = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const io = new Server(PORT, {
   cors: true,
 });
 
 const emailToSocketMap = new Map();
 const socketidToEmailMap = new Map();
+
 io.on("connection", (socket) => {
-  console.log(`Socket Conneted`, socket.id);
+  console.log(`Socket Connected`, socket.id);
+
   socket.on("room:join", (data) => {
     const { email, room } = data;
     emailToSocketMap.set(email, socket.id);
@@ -17,6 +19,7 @@ io.on("connection", (socket) => {
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
   });
+
   socket.on("user:call", ({ to, offer }) => {
     io.to(to).emit("incoming:call", { from: socket.id, offer });
   });
@@ -31,5 +34,9 @@ io.on("connection", (socket) => {
 
   socket.on("peer:nego:done", ({ to, ans }) => {
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+  socket.on("send:message", ({ to, message, timestamp }) => {
+    const fromEmail = socketidToEmailMap.get(socket.id);
+    io.to(to).emit("receive:message", { from: fromEmail, message, timestamp });
   });
 });
